c56a78787f79766c3a636a6c414ab47087a0211d
48panda48 <58887107+48panda48@users.noreply.github.com>
2020-04-02 15:12:37 +0100

    added testv1

---
+++ testv1.py
@@ -0,0 +1,2 @@
+import gitAutoUpdate
+gitAutoUpdate.gitAutoUpdate("https://raw.githubusercontent.com/48panda48/Self-Update/master/code.txt","https://raw.githubusercontent.com/48panda48/Self-Update/master/version.txt?token=AOBIXQ3GAMS3PLRVC4LCOAC6QSBLO","v1",[""],"test")
